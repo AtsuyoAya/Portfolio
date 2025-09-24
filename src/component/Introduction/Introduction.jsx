2fb33e284a571a0bfd824c24a6f6e05aa59f4b2d
AtsuyoAya <AnootherLife@gmail.com>
2025-09-24 10:40:04 +0200

    Portfolio

--- src/component/Introduction/Introduction.jsx
+++ src/component/Introduction/Introduction.jsx
@@ -4,7 +4,7 @@ function Introduction({ }) {
 
     return (
         <div className={"Introduction"}>
-            Bonjour, je m'appelle <b>William Kientz-Guaydamour</b>, j'ai 22 ans et je suis à la recherche d'une entreprise pour mon alternance en <b>développement web</b>. Titulaire d'un <b>baccalauréat professionnel Systèmes numériques option Informatique et Réseaux</b>, je cherche à développer des connaissances en <b>développement web</b>. J'ai peu de connaissances en code mais je possède de légères bases en <b>HTML / CSS / REACT et C++</b>.
+            Bonjour, je m'appelle <b>William Kientz-Guaydamour</b>, j'ai 22 ans et je suis à la recherche d'une entreprise pour mon alternance en <b>développement web spécialité Unity ou IA/Nocode</b>. Titulaire d'un <b>baccalauréat professionnel Systèmes numériques option Informatique et Réseaux</b>, je cherche à développer des connaissances en <b>développement web</b>. J'ai peu de connaissances en code mais je possède de légères bases en <b>HTML / CSS / REACT et C++</b>.
 
 
         </div>
